4e78fb2c6ec957e0b4615bca69aac816a09e1489
Aditya Agarwal <21240581+naveenadi@users.noreply.github.com>
2022-08-28 16:20:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.post('/', checkUser ,(req, res) => {
     res.send("You are logged in")
 })
 
+const PORT = process.env.PORT || 3000
 
-app.listen(3000,() => {
+app.listen(PORT,() => {
     console.log("This server is start at the port number 3000")
 })
